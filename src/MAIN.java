34358accba7d0cedee3c489ed30ba3eeb9075652
Dima <028dima@gmail.com>
2021-10-26 20:54:25 +0300

    first commit

---
+++ src/MAIN.java
@@ -0,0 +1,27 @@
+public class MAIN {
+    public static void main (String[] args){
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5f;
+        double myDoubleValue = 5d;
+        System.out.println("MyIntValue = "+ myIntValue);
+        System.out.println("MyFloatValue = "+ myFloatValue);
+        System.out.println("MyDoubleValue = "+ myDoubleValue);
+
+        double numbersOfPounds = 200d;
+        double convertedKilograms = numbersOfPounds * 0.45359237d;
+        System.out.println("Converted kilograms= " + convertedKilograms);
+    }
+
+}
